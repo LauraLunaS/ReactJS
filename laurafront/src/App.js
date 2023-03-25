3935c8c0e09e9445f6a7a0f9ff57fa5c3c7a9d45
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-03-25 00:06:53 -0300

    props e component

--- laurafront/src/App.js
+++ laurafront/src/App.js
@@ -1,21 +1,40 @@
 import './App.css';
 
-function Bemvindo (props) {
+function Equipe (props) {
   return(
     <div>
-      <p>Bem-Vindo {props.nome}</p>
-      <p>Tenho {props.idade} Anos</p>
+      <Sobre username={props.nome} cargo={props.cargo} idade={props.idade} />
+      <Social fb={props.facebook} lk={props.linkedin} yt={props.youtube} />
     </div>
   );
 }
 
+function Sobre (props) {
+  return (
+    <div>
+      <p>Olá sou o(a) {props.username}</p>
+      <p>Cargo: {props.cargo}</p>
+      <p>Idade: {props.idade} anos</p>
+    </div>
+  )
+}
+
+function Social (props) {
+  return (
+    <div>
+      <a href={props.fb}>Facebook</a>
+      <a href={props.lk}>Linkedin</a>
+      <a href={props.yt}>Youtube</a>
+    </div>
+
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      < Bemvindo nome="Laura" idade="18"/>
-      < Bemvindo nome="Leticia" idade="20"/>
+      <h1>Conheça nossa equipe:</h1>
+        <Equipe nome="Laura" cargo="Programadora" idade="18" facebook="https://pt-br.facebook.com/" linkedin="https://br.linkedin.com/" youtube="https://www.youtube.com/"/>
     </div>
   );
 }
