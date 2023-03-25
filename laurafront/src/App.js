3ffe305fb80745efa37875c4de72b53ccd974f4c
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-03-25 16:00:11 -0300

    forms

--- laurafront/src/App.js
+++ laurafront/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Feed from './components/Feed'
 import './App.css';
 
 
@@ -8,27 +7,46 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      feed:[
-        {id: 1, username: 'Laura', curtidas:10, comentarios:2},
-        {id: 2, username: 'Laura', curtidas:20, comentarios:4},
-        {id: 3, username: 'Laura', curtidas:30, comentarios:8},
-        {id: 4, username: 'Laura', curtidas:1, comentarios:1},
-      ]
+      email: '',
+      senha: '',
+      sexo: 'masculino'
     }
+
+    this.trocaEmail = this.trocaEmail.bind(this);
+    this.trocaSexo = this.trocaSexo.bind(this);
+  }
+
+  trocaEmail(e){
+    let valorDigitado = e.target.value;
+    this.setState({email: valorDigitado});
+  }
+
+  trocaSexo(e){
+    let valorDigitado = e.target.value;
+    this.setState({sexo: valorDigitado})
   }
 
   render(){
     return (
       <div>
-        
-        {this.state.feed.map((item)=>{
-          return(
-            < Feed id={item.id} username={item.username} curtidas={item.curtidas} comentarios={item.comentarios}/>
-          );
-        })}
-       
+        <h3>Login</h3>
+        Email:
+        <input type="email" name="email" value={this.state.email} onChange={this.trocaEmail}/>
+        Senha: 
+        <input type="password" name="senha" value={this.state.senha}  onChange={(e) => this.setState({senha: e.target.value})} />
+        Sexo:
+        <select name='sexo' value={this.state.sexo} onChange={this.trocaSexo}>
+          <option value="Masculino">Maculino</option>
+          <option value="Feminino">Feminino</option>
+        </select>
 
+        <div>
+          <h4>{this.state.email}</h4>
+          <h4>{this.state.senha}</h4>
+          <h4>{this.state.sexo}</h4>
+        </div>
       </div>
+      
       
     );
   }
