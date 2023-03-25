8e8044fc23156128b5050937fecdf0b223fcf14a
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-03-25 02:33:34 -0300

    renderização

--- laurafront/src/App.js
+++ laurafront/src/App.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
-import Membro from './Components/Membro';
 import './App.css';
 
 
 class App extends Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      status: false
+    };
+
+    this.entrar = this.entrar.bind(this);
+    this.sair = this.sair.bind(this);
+    
+  }
+
+  sair(){
+    this.setState({ status: false });
+  } 
+  
+  entrar(){
+    this.setState({ status: true });
+  }
+
+ 
+
 
   render(){
     return (
       <div>
-        < Membro />
+        {this.state.status ?
+        <div>
+          <h3>Bem-Vindo ao sistema</h3>
+          <button onClick={this.sair}>Sair</button>
+        </div> :
+        <div>
+            <h3>Olá Visitante, faça o login</h3>
+            <button onClick={this.entrar}>Entrar no sistema</button>
+        </div>
+        }
+
       </div>
+      
     );
   }
 }    
