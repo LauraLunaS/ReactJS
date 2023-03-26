e6f68208b6a67ff37c17ccdea737bc12f9cc2fe9
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-03-25 21:32:48 -0300

    UseState

--- laurafront/src/App.js
+++ laurafront/src/App.js
@@ -1,65 +1,34 @@
-import React, {Component} from 'react';
-import cronometro from './assets/cronometro.png'; 
-import './style.css'
+import React, { useState } from 'react';
 
 
-class App extends Component{
+function App() {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      numero: 0,
-      botao: 'VAI'
-    };
-    this.timer = null;
-    this.vai = this.vai.bind(this);
-    this.limpar = this.limpar.bind(this);
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React Hooks'
+  ]);
+  const [input, setInput] = useState('');
+
+  function handleAdd(){
+    setTarefas([...tarefas, input])
+    setInput('');
   }
 
-  vai(){
-    let state = this.state;
 
-    if(this.timer !== null){
-      clearInterval(this.timer);
-      this.timer = null;
-      state.botao = 'VAI';
-    }else{
-      this.timer = setInterval(()=>{
-        let state = this.state;
-        state.numero += 0.1;
-        this.setState(state);
-      },100);
-      state.botao = 'PAUSAR';
-    }
+  return (
+    <div>
 
-    this.setState(state);
-  }
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
 
-  limpar(){
-    if(this.timer !== null){
-      clearInterval(this.timer);
-      this.timer = null;
-    }
+      <input type="text" value={input} onChange={e => setInput(e.target.value)}/>    
+      <button type="button" onClick={handleAdd}>Adicionar</button>
 
-    let state = this.state;
-    state.numero = 0;
-    state.botao = 'VAI';
-    this.setState(state);
-
-  }
-
-  render(){
-    return(
-      <div className="container">
-        <img src={cronometro} className="img" />
-        <a className="timer">{this.state.numero.toFixed(1)}</a>
-        <div className="areaBtn">
-          <a className="botao" onClick={this.vai}>{this.state.botao}</a>
-          <a className="botao" onClick={this.limpar}>LIMPAR</a>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
