c8bf3c54952ecf2167b1177d9dbff0c15dec2d6a
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-04-01 17:41:48 -0300

    listas com firebase

--- firebase/src/App.js
+++ firebase/src/App.js
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { db } from './firebaseConnection';
-import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore'
+import { doc, setDoc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore'
 
 import './app.css';
 
 function App() {
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
+
+  const [posts, setPosts] = useState([]);
 
 
   async function handleAdd(){
@@ -40,18 +42,37 @@ function App() {
 
 
   async function buscarPost(){
-    
-    const postRef = doc(db, "posts", "vFvZAyFqebXFsFv0X89l")
+    // const postRef = doc(db, "posts", "vFvZAyFqebXFsFv0X89l")
+    // await getDoc(postRef)
+    // .then((snapshot) => {
+    //   setAutor(snapshot.data().autor)
+    //   setTitulo(snapshot.data().titulo)
 
-    await getDoc(postRef)
+    // })
+    // .catch(()=>{
+    //   console.log("ERRO AO BUSCAR")
+    // })
+
+    const postsRef = collection(db, "posts")
+    await getDocs(postsRef)
     .then((snapshot) => {
-      setAutor(snapshot.data().autor)
-      setTitulo(snapshot.data().titulo)
+      let lista = [];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        })
+      })
+
+      setPosts(lista);
 
     })
-    .catch(()=>{
-      console.log("ERRO AO BUSCAR")
+    .catch((error) => {
+      console.log("DEU ALGUM ERRO AO BUSCAR")
     })
+
 
   }
 
@@ -78,6 +99,17 @@ function App() {
 
       <button onClick={handleAdd}>Cadastrar</button>
       <button onClick={buscarPost}>Buscar post</button>
+
+      <ul>
+        {posts.map( (post) => {
+          return(
+            <li key={post.id}>
+              <span>Titulo: {post.titulo} </span> <br/>
+              <span>Autor: {post.autor}</span> <br/>  <br/>
+            </li>
+          )
+        })}
+      </ul>
 
     </div>
 
