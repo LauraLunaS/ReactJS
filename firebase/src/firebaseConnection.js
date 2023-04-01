ff32d4102c88dfac2133793f183fda165f551e20
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-04-01 17:15:54 -0300

    database

---
+++ firebase/src/firebaseConnection.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3KEnC7_f-cLToF5JqoBlOQXZYodlLmE8",
+  authDomain: "test-2e0a1.firebaseapp.com",
+  projectId: "test-2e0a1",
+  storageBucket: "test-2e0a1.appspot.com",
+  messagingSenderId: "920581333245",
+  appId: "1:920581333245:web:39aa0082d888562a429e94",
+  measurementId: "G-WE5121B8P9"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
