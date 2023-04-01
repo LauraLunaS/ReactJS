3a5370c80551aa1baa963a098692c3dcf67dee2e
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-04-01 17:58:00 -0300

    remove post list

--- firebase/src/App.js
+++ firebase/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { db } from './firebaseConnection';
-import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc } from 'firebase/firestore'
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc } from 'firebase/firestore'
 
 import './app.css';
 
@@ -98,6 +98,16 @@ function App() {
 
   }
 
+
+  async function excluirPost(id){
+    const docRef = doc(db, "posts", id)
+    await deleteDoc(docRef)
+    .then(() =>{
+      alert("POST DELETADO COM SUCESSO!")
+    })
+
+  }
+
   return (
     <div>
       <h1>ReactJS + Firebase :)</h1>
@@ -139,7 +149,8 @@ function App() {
             <li key={post.id}>
               <strong>ID: {post.id}</strong> <br/>
               <span>Titulo: {post.titulo} </span> <br/>
-              <span>Autor: {post.autor}</span> <br/>  <br/>
+              <span>Autor: {post.autor}</span> <br/> 
+              <button onClick={ () => excluirPost(post.id) }>Excluir</button> <br/> <br/>
             </li>
           )
         })}
