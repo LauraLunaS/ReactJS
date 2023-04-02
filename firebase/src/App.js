e7e569c7d7d35180028f8c05ac19530ea7830830
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-04-01 22:00:05 -0300

    home e register

--- firebase/src/App.js
+++ firebase/src/App.js
@@ -1,11 +1,10 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom'
 import RoutesApp from './routes'
-
 
 export default function App(){
   return(
     <BrowserRouter>
-      <RoutesApp />
+      <RoutesApp/>
     </BrowserRouter>
   )
 }
--- firebase/src/firebaseConnection.js
+++ firebase/src/firebaseConnection.js
@@ -1,24 +1,21 @@
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from 'firebase/auth';
-
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3KEnC7_f-cLToF5JqoBlOQXZYodlLmE8",
-  authDomain: "test-2e0a1.firebaseapp.com",
-  projectId: "test-2e0a1",
-  storageBucket: "test-2e0a1.appspot.com",
-  messagingSenderId: "920581333245",
-  appId: "1:920581333245:web:39aa0082d888562a429e94",
-  measurementId: "G-WE5121B8P9"
+  apiKey: "AIzaSyBx9WFTQC2XIi-4xE1adOMQYaxEEeshZFg",
+  authDomain: "cursoapp-f0398.firebaseapp.com",
+  projectId: "cursoapp-f0398",
+  storageBucket: "cursoapp-f0398.appspot.com",
+  messagingSenderId: "394026650841",
+  appId: "1:394026650841:web:2072c9f66868dfd41ac2c8",
+  measurementId: "G-JST0NMT5EC"
 };
 
-// Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const db = getFirestore(firebaseApp);
-const auth = getAuth(firebaseApp);
+const auth = getAuth(firebaseApp)
 
 export { db, auth };
