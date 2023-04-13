bcb366a83d0b89e943cd31fa9d0fa8e71c127e43
Laura Luna <lauraluna.siqueira@hotmail.com>
2023-04-13 16:41:09 -0300

    styles main

--- projeto-repos/src/pages/Main/index.js
+++ projeto-repos/src/pages/Main/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+import {Container, Form, SubmitButton} from './styles';
 
-import {Title} from './styles';
+
 
 export default function Main(){
     return(
-        <Title>
-            Main
-        </Title>
+        <Container>
+            <h1>
+            <FaGithub size={25}/>
+            Meus Repositorios
+            </h1>
+
+            <Form onSubmit={()=>{}}>
+            <input type="text" placeholder="Adicionar Repositorios"/>
+
+            <SubmitButton>
+            <FaPlus color="#FFF" size={14}/>
+            </SubmitButton>
+
+            </Form>
+        </Container>
     )
 }
